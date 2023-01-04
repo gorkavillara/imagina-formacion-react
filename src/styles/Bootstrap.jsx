807f4f3da19d7a4528451a0190e3003f711b31ae
gorkavillar <gorka@gorkavillar.com>
2023-01-04 11:14:01 +0100

    ⛓️ Enlaces CDN de Bootstrap en index.html

--- src/styles/Bootstrap.jsx
+++ src/styles/Bootstrap.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react"
 
-const Bootstrap = () => {
+const BootstrapView = () => {
   return (
-    <div>Bootstrap</div>
+    <div className="card">
+      <div className="card-header">React con Bootstrap</div>
+      <div className="card-body">
+        <h5 className="card-title">Librería de UI con componentes</h5>
+        <p className="card-text">
+          Bootstrap es una de las librerías más utilizadas en web
+        </p>
+        <a href="#" className="btn btn-primary">
+          Enlace
+        </a>
+      </div>
+    </div>
   )
 }
 
-export default Bootstrap
+export default BootstrapView
