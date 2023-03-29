22a345474fa120b29c87d0343b73e78c3a3394e4
gorkavillar <gorka@gorkavillar.com>
2023-03-29 16:13:37 +0200

    🅱️ Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import "./App.css";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchPosts } from "./features/postsSlice";
-
+import Bootstrap from "./components/Bootstrap";
 
 function App() {
-  const dispatch = useDispatch()
-  const posts = useSelector((state) => state.posts.posts);
-  const status = useSelector((state) => state.posts.status);
-  const error = useSelector((state) => state.posts.error);
-
-  const buscarPosts = () => {
-    dispatch(fetchPosts())
-  }
   return (
-    <div>
-      {status === 'loading' && <div>Cargando...</div>}
-      {status === 'failed' && <div>{error}</div>}
-      {posts.map((post) => (
-        <div key={post.id}>{post.title}</div>
-      ))}
-      <button onClick={buscarPosts}>Buscar Posts</button>
-    </div>
+    <Bootstrap />
   );
 }
 
---
+++ src/components/Bootstrap.jsx
@@ -0,0 +1,14 @@
+function Bootstrap() {
+  return (
+    <div className="container mt-2">
+      <div className="spinner-border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+      <div className="spinner-grow" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
+}
+
+export default Bootstrap;
