40bfa43c25ed77c66c0fccad04e84348eb9761e6
gorkavillar <gorka@gorkavillar.com>
2023-04-09 18:58:53 +0200

    Variables de entorno

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ function App() {
   return (
     <div>
       <p>Count: {count}</p>
-      <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
+      <button onClick={() => setCount((prev) => prev + 1)}>
+        Increment
+      </button>
+      <h4>Url: {process.env.REACT_APP_API_URL}</h4>
+      <p>Entorno: {process.env.NODE_ENV}</p>
     </div>
   );
 }
