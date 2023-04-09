9c5325d817fd1fe48cf3069d68bb30855c49d340
gorkavillar <gorka@gorkavillar.com>
2023-04-09 17:14:45 +0200

    Utilizando useEffect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback } from "react";
+import ComponenteUseMemo from "./components/ComponenteUseMemo";
+import ComponenteUseEffect from "./components/ComponenteUseEffect";
 
-function MyComponent() {
+const App = () => {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState("Gorka");
 
   const increment = useCallback(() => {
-    setCount(count + 1);
-  }, [count]);
+    // setCount(count + 1);
+    setCount(prev => prev + 1)
+  }, []);
 
   return (
     <div>
-      <p>Count: {count}</p>
-      <button onClick={increment}>Increment</button>
+      <p>Contador: {count}</p>
+      <button onClick={increment}>Sumar</button>
+      <button onClick={() => setName("Julián")}>Cambiar Nombre</button>
+      <ComponenteUseMemo nombre={name} />
+      {count < 5 && <ComponenteUseEffect />}
     </div>
   );
 }
+
+export default App
